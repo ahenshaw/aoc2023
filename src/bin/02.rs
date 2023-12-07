da42821554d57e4c05b89297b186aabe68b0f018
Andy Henshaw <andy@nexuslabs.dev>
2023-12-07 10:59:19 -0500

    added example data to repo

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -68,13 +68,17 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", 2));
+        let result = part_one(&advent_of_code::template::read_file_with_part(
+            "examples", 2, 1,
+        ));
         assert_eq!(result, Some(8));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", 2));
+        let result = part_two(&advent_of_code::template::read_file_with_part(
+            "examples", 2, 2,
+        ));
         assert_eq!(result, Some(2286));
     }
 }
